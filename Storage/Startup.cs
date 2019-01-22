358e6d72018264785db0b94b06c44145318928f7
John Lundgren <john.paul.lundgren@gmail.com>
2019-01-22 19:21:10 +0100

    Added Connection String for MACOS

--- Storage/Startup.cs
+++ Storage/Startup.cs
@@ -37,7 +37,7 @@ namespace Storage
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddDbContext<StorageContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("StorageContext")));
+                    options.UseSqlServer(Configuration.GetConnectionString("StorageContextMac")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
